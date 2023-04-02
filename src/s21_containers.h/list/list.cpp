5cafa44b9164f1cb5553323e57e0a6b3193f42ca
lunarskii <lunarskii0@gmail.com>
2023-04-02 13:07:58 +0300

    added Node and ListIterator classes, as well as a few operator overloads

--- src/s21_containers.h/list/list.cpp
+++ src/s21_containers.h/list/list.cpp
@@ -1,120 +1,125 @@
 #include "list.h"
 
-Node::Node(double data) {
-    this->data = data;
-    this->prev = this->next = nullptr;
-}
+template<typename value_type>
+List<value_type>::Node::Node(const value_type& d, Node* p, Node* n) : data(d), prev(p), next(n) {}
 
-S21List::S21List() : head(nullptr), tail(nullptr) {}
+template<typename value_type>
+List<value_type>::List() : head(nullptr), tail(nullptr) {}
 
-S21List::~S21List() {
+template<typename value_type>
+List<value_type>::~List() {
     while (head != nullptr) pop_front();
 }
 
-Node* S21List::push_front(double data) {
-    Node *temp = new Node(data);
-    
-    temp->next = head;
-    if (head != nullptr) head->prev = temp;
-    if (tail == nullptr) tail = temp;
-    head = temp;
-
-    return temp;
-}
-
-Node* S21List::push_back(double data) {
-    Node *temp = new Node(data);
-
-    temp->prev = tail;
-    if (tail != nullptr) tail->next = temp;
-    if (head == nullptr) head = temp;
-    tail = temp;
-
-    return temp;
-}
-
-void S21List::pop_front() {
-    if (head != nullptr) {
-        Node *temp = head->next;
-
-        if (temp != nullptr)
-            temp->prev = nullptr;
-        else
-            tail = nullptr;
-
-        delete head;
-        head = temp;
+template<typename value_type>
+void List<value_type>::push_front(value_type& data) {
+    if (head == nullptr) {
+        head = tail = new Node(data);
+    } else {
+        head->prev = new Node(data, nullptr, head);
+        head = head->prev;
     }
 }
 
-void S21List::pop_back() {
-    if (tail != nullptr) {
-        Node *temp = tail->prev;
+template<typename value_type>
+void List<value_type>::push_back(const value_type& data) {
+    if (tail == nullptr) {
+        tail = head = new Node(data);
+    } else {
+        tail->next = new Node(data, tail);
+        tail = tail->next;
+    }
+}
 
-        if (temp != nullptr)
-            temp->next = nullptr;
+template<typename value_type>
+void List<value_type>::pop_front() {
+    if (head != nullptr) {
+        Node *tempHead = head;
+
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr;
+        }
+
+        delete tempHead;
+    }
+}
+
+template<typename value_type>
+void List<value_type>::pop_back() {
+    if (tail != nullptr) {
+        Node *temp = tail;
+
+        tail = tail->prev;
+        if (tail != nullptr)
+            tail->next = nullptr;
         else
             head = nullptr;
 
-        delete tail;
-        tail = temp;
+        delete temp;
     }
 }
 
-Node* S21List::getAt(int index) {
-    Node* ptr = head;
-    int n = 0;
+// template<typename value_type>
+// typename S21List<value_type>::Node* S21List<value_type>::getAt(int index) {
+//     Node* ptr = head;
+//     int n = 0;
 
-    while (n != index) {
-        if (ptr == nullptr) return ptr;
-        ptr = ptr->next;
-        n++;
-    }
+//     while (n != index) {
+//         if (ptr == nullptr) return ptr;
+//         ptr = ptr->next;
+//         n++;
+//     }
 
-    return ptr;
-}
+//     return ptr;
+// }
 
-Node* S21List::operator[](int index) {
-    return getAt(index);
-}
+// template<typename value_type>
+// typename S21List<value_type>::Node* S21List<value_type>::operator[](int index) {
+//     return getAt(index);
+// }
 
-Node* S21List::insert(int index, double data) {
-    Node* right = getAt(index);
-    if (right == nullptr)
-        return push_back(data);
+// template<typename value_type>
+// typename S21List<value_type>::Node* S21List<value_type>::insert(int index, double data) {
+//     Node* right = getAt(index);
+//     if (right == nullptr)
+//         return push_back(data);
 
-    Node* left = right->prev;
-    if (left == nullptr)
-        return push_front(data);
+//     Node* left = right->prev;
+//     if (left == nullptr)
+//         return push_front(data);
 
-    Node* ptr = new Node(data);
+//     Node* ptr = new Node(data);
     
-    ptr->prev = left;
-    ptr->next = right;
-    left->next = ptr;
-    right->prev = ptr;
+//     ptr->prev = left;
+//     ptr->next = right;
+//     left->next = ptr;
+//     right->prev = ptr;
 
-    return ptr;
-}
+//     return ptr;
+// }
 
-void S21List::erase(int index) {
-    Node* ptr = getAt(index);
-    if (ptr == nullptr) return;
+// template<typename value_type>
+// void S21List<value_type>::erase(int index) {
+//     Node* ptr = getAt(index);
+//     if (ptr == nullptr) return;
 
-    if (ptr->prev == nullptr) {
-        pop_front();
-        return;
-    }
+//     if (ptr->prev == nullptr) {
+//         pop_front();
+//         return;
+//     }
 
-    if (ptr->next == nullptr) {
-        pop_back();
-        return;
-    }
+//     if (ptr->next == nullptr) {
+//         pop_back();
+//         return;
+//     }
 
-    Node* left = ptr->prev;
-    Node* right = ptr->next;
-    left->next = right;
-    right->prev = left;
+//     Node* left = ptr->prev;
+//     Node* right = ptr->next;
+//     left->next = right;
+//     right->prev = left;
 
-    delete ptr;
-}
+//     delete ptr;
+// }
--- src/s21_containers.h/list/list.h
+++ src/s21_containers.h/list/list.h
@@ -1,33 +1,76 @@
 #ifndef SRC_S21_CONTAINERS_H_LIST_LIST_H_
 #define SRC_S21_CONTAINERS_H_LIST_LIST_H_
 
-// не могу никак без public реализовать
-class Node {
+#include <cstddef>
+
+template <typename T>
+class List {
     public:
-        Node(double data);
-        double data;
+        class Node;
+        class ListIterator;
+        class ListConstIterator;
+    
+        using value_type = T;
+        using reference = T&;
+        using const_reference = const T&;
+        using iterator = List<T>::ListIterator;
+        // using const_iterator = List<const T>::ListConstIterator;
+        using size_type = size_t;
+
+
+        /*                  CONSTRUCTORS/DESTRUCTORS                                            */
+        List();                                              // default constructor, creates an empty list
+        ~List();                                             // destructor
+
+        void erase(int index);
+        // typename S21List<T>::Node* insert(int index, double data);
+        void pop_back();
+        void pop_front();
+        void push_back(const value_type& data);
+        void push_front(value_type& data);
+    private:
+        Node *head;
+        Node *tail;
+};
+
+template <typename value_type>
+class List<value_type>::Node {
+    public:
+        Node(const value_type& d = value_type{}, Node* p = nullptr, Node* n = nullptr);
+
+    private:
+        value_type data;
         Node *prev;
         Node *next;
 };
 
-class S21List {
+template <typename value_type>
+class List<value_type>::ListIterator {
     public:
-        S21List();   // constructor
-        ~S21List();  // destructor
+        // Конструктор по умолчанию
+        ListIterator() = default;
 
-        Node *push_front(double data);
-        Node *push_back(double data);
-        void pop_front();
-        void pop_back();
-        Node* getAt(int index);
-        Node* insert(int index, double data);
-        void erase(int index);
+        // Конструктор с указанием начального узла
+        ListIterator(Node *node);
 
-        Node* operator[](int index);
+        // Операторы инкремента и декремента
+        ListIterator& operator++();
+        ListIterator& operator--();
+        ListIterator operator++(int);
+        ListIterator operator--(int);
 
+        // Операторы сравнения
+        bool operator==(const ListIterator& other) const;
+        bool operator!=(const ListIterator& other) const;
+            
     private:
-        Node *head;
-        Node *tail;
+        Node* node_{nullptr};
+};
+
+template <typename value_type>
+class List<value_type>::ListConstIterator {
+    public:
+    private:
 };
 
 #endif  // SRC_S21_CONTAINERS_H_LIST_LIST_H_
