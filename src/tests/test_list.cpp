135345c72f4e75188911e6193f7d442e081d1ca2
lunarskii <lunarskii0@gmail.com>
2023-04-03 15:24:42 +0300

    added a test for the constructor

--- src/tests/test_list.cpp
+++ src/tests/test_list.cpp
@@ -1,1 +1,15 @@
 #include "test_core.h"
+#include <list>
+
+using namespace std;
+TEST(CONSTRUCTORS, DEFAULT_CONSTRUCTOR) {
+    List<int> x = {1, 2, 3};
+    list<int> y = {1, 2, 3};
+
+    List<int>::iterator it_x = x.begin();
+    list<int>::iterator it_y = y.begin();
+    for (; it_x != x.end() || it_y != y.end(); ++it_x, ++it_y) {
+        ASSERT_EQ(*it_x, *it_y);
+    }
+}
+
