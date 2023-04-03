5f1bd2a46594a885f2af0d61964beec340b5cf0b
lunarskii <lunarskii0@gmail.com>
2023-04-03 21:47:06 +0300

    libraries moved to src

---
+++ src/s21_containers.h
@@ -0,0 +1,11 @@
+#ifndef SRC_S21_CONTAINERS_H_S21_CONTAINERS_H_
+#define SRC_S21_CONTAINERS_H_S21_CONTAINERS_H_
+
+#include "s21_containers/list/list.h"
+#include "s21_containers/map/map.h"
+#include "s21_containers/queue/queue.h"
+#include "s21_containers/set/set.h"
+#include "s21_containers/stack/stack.h"
+#include "s21_containers/vector/vector.h"
+
+#endif  // SRC_S21_CONTAINERS_H_S21_CONTAINERS_H_
